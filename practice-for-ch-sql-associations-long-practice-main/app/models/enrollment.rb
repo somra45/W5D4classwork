2099e8ce9b48bcef38bcdb74c88e720ab88b2a5b
root <somra45@gmail.com>
2023-04-06 13:57:50 -0400

    enrollment and user associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/enrollment.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    belongs_to :course, 
+        class_name: :Course, 
+        foreign_key: :course_id, 
+        primary_key: :id
+
+    belongs_to :student, 
+        class_name: :User, 
+        foreign_key: :student_id, 
+        primary_key: :id
 end
